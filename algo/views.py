aaaa985189c83de8da547f99c6b28d3222f6421a
joro2404 <joro2404@abv.bg>
2018-01-20 21:16:16 +0200

    Add files via upload
    these 2 !!

--- algo/views.py
+++ algo/views.py
@@ -1,6 +1,9 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.http import HttpRequest
+from django.contrib import messages
 def index(request):
-    param = request.GET.get('q', '')
-    print(param)
+    parm=request.GET.get('q','')
+    print(parm)
     return HttpResponse(request)
+    messages.add_message(request, messages.INFO, parm)
